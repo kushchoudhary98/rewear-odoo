ff4abce832cc826910ce2982a330ca94f8adb4af
Kush Choudhary <kushchoudhary8430@gmail.com>
2025-07-12 10:24:43 +0530

    added firebase

---
+++ app/firebase/config.tsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDr2j97PXgE4Z20kZY8NQmVurm45Bnlmm0",
+  authDomain: "rewear-kc-odoo.firebaseapp.com",
+  projectId: "rewear-kc-odoo",
+  storageBucket: "rewear-kc-odoo.firebasestorage.app",
+  messagingSenderId: "737280390554",
+  appId: "1:737280390554:web:f971c0522768966943b623",
+  measurementId: "G-H1H1M8QRMH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
